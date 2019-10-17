a0118b9de1aa0f39449e030d8d9b0361b8597ad7
Jakub Wietecha <estissy@gmail.com>
2019-10-17 14:43:06 +0200

    Added helper function

--- femos/core.py
+++ femos/core.py
@@ -7,3 +7,13 @@ def get_random_numbers(quantity, lower_threshold, upper_threshold):
         numbers.append(uniform(lower_threshold, upper_threshold))
 
     return numbers
+
+
+def get_number_of_nn_weights(input_nodes, hidden_layers_nodes, output_nodes):
+    grouped_nodes = [input_nodes] + hidden_layers_nodes + [output_nodes]
+
+    total = 0
+    for index in range(len(grouped_nodes) - 1):
+        total += grouped_nodes[index] * grouped_nodes[index + 1]
+
+    return total
