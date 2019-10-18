0cb50bfc47113c41c561425f69e879b8f4e19391
Jakub Wietecha <estissy@gmail.com>
2019-10-18 14:14:26 +0200

    Added basic evolution algorithm

--- femos/core.py
+++ femos/core.py
@@ -17,3 +17,26 @@ def get_number_of_nn_weights(input_nodes, hidden_layers_nodes, output_nodes):
         total += grouped_nodes[index] * grouped_nodes[index + 1]
 
     return total
+
+
+def get_next_population(population, phenotype_strategy, evaluation_strategy, parent_selection_strategy,
+                        mutation_strategy, offspring_selection_strategy):
+    phenotypes = map(phenotype_strategy, population)
+    phenotypes_values = evaluation_strategy(list(phenotypes))
+
+    parent_indices = parent_selection_strategy(phenotypes_values)
+    parents = map(lambda parent_index: population[parent_index], parent_indices)
+
+    mutated_parents = map(mutation_strategy, parents)
+    offspring = offspring_selection_strategy(population, mutated_parents)
+
+    return offspring
+
+
+def get_evolved_population(initial_population, phenotype_strategy, evaluation_strategy, parent_selection_strategy,
+                           mutation_strategy, offspring_selection_strategy, number_of_epochs):
+    tmp_population = initial_population
+
+    for number_of_epochs in range(number_of_epochs):
+        tmp_population = get_next_population(tmp_population, phenotype_strategy, evaluation_strategy,
+                                             parent_selection_strategy, mutation_strategy, offspring_selection_strategy)
