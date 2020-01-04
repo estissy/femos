5223443c821d1bf033d44fdea20692869d37fbda
Jakub Wietecha <estissy@gmail.com>
2020-01-04 11:16:49 +0100

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="femos",
-    version="0.0.4",
+    version="0.0.5",
     author="estissy",
     author_email="estissy@gmail.com",
     description="My small library for neuroevolution",
